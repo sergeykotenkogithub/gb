cf7ebbe91a67dcfe9e20fba19d7e7c008865f232
sergeykotenko <sergeykotenko@bk.ru>
2021-05-11 16:30:11 +0300

    first commit

---
+++ site.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title><?=$title?></title>
+        <meta charset="UTF-8">
+        <link rel="stylesheet" type="text/css" href="style.css" />
+    </head>
+    <body>
+        <section>
+            <h1><?=$about?></h1>
+            Краткая биография обо мне
+            Родился в 1985 году в городе Москва. Закончил в 2008 году МАИ.
+            На данный момент работаю ведущим инженером в крупной авиакомпании.
+            Поскольку я люблю авиацию, то хотел бы поделиться несколькими интересными
+            фотографиями на эту тему
+            <br><br>
+            <img src="img/work.jpg" alt="work">
+            <br><br>
+            <br><br>
+        </section>
+        <footer>
+            <b>Просто пример жирного текста</b>
+            <br><br>
+            <?=$year?>
+        </footer>
+    </body>
+</html>
