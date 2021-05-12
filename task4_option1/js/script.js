0d8f24d9f4c5f00718e1a2aff5db5b8a9abcbb58
sergeykotenko <sergeykotenko@bk.ru>
2021-05-12 22:57:38 +0300

    all task

--- task4_option1/js/script.js
+++ task4_option1/js/script.js
@@ -7,7 +7,4 @@ const swiper = new Swiper('.swiper-container', {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev',
     },
-    // scrollbar: {
-    //     el: '.swiper-scrollbar',
-    // },
 });
---
+++ task4_option2/index.php
@@ -0,0 +1,16 @@
+
+<?php
+
+// Задание 4. 2й Способ:
+
+const IMG = "img/";
+$title = "Главная страница - страница обо мне";
+$about = "Информация обо мне";
+$favicon = "1.png";
+$img_1 = "work1.jpg";
+$img_2 = "work2.jpg";
+$img_3 = "work3.jpg";
+$img_4 = "work4.jpg";
+$current_year = date ( 'Y' );
+
+include "site.php";
