37031289083e8ed23e260dee7b337acc4553b44f
sergeykotenko <sergeykotenko@bk.ru>
2021-05-31 02:42:13 +0300

    lesson6

--- engine/getFileImg.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-// Вывод картинок gallery_img/big/
-function getImages() {
-    return $files = array_splice( scandir(IMG_BIG), 2);
-}
-$getImages = getImages();
---
+++ site/engine/render.php
@@ -0,0 +1,24 @@
+<?php
+
+function renderTemplate($page, $params = []) {
+    extract($params);
+
+    ob_start();
+    $filename = TEMPLATES_DIR . $page . ".php";
+    if(file_exists($filename)) {
+        include $filename;
+    }
+
+    return ob_get_clean();
+}
+
+function render ($page, $params) {
+
+
+    return renderTemplate(LAYOUT_DIR . $params['layout'], [
+        'menu' => renderTemplate('menu', $params),
+        'content' => renderTemplate($page, $params),
+        'title' => $params['title'],  // Заголок который находится в main.php
+        'gall' => $params['gall'],
+    ]);
+}
